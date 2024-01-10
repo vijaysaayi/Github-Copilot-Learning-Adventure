05c03e6986f7b4dc71cc78e3403e9735b2538ade
raorugan <raorugan@microsoft.com>
2024-01-10 15:53:24 +0530

    Update Challenge5Tests.cs

--- csharp/Challenges.Evaluation/Challenge5Tests.cs
+++ csharp/Challenges.Evaluation/Challenge5Tests.cs
@@ -4,9 +4,9 @@ public class Challenge5Tests
 {
     // Goal : What should be the input so that this test case passes ?
     [Theory]
-    [InlineData("Test-Case-1 : Provide-some-input")]
-    [InlineData("Test-Case-2 : Provide-some-input")]
-    [InlineData("Test-Case-3 : Provide-some-input")]
+    [InlineData("Provide-some-input")]
+    [InlineData("Provide-some-input")]
+    [InlineData("Provide-some-input")]
     public void YouNeedToProvideTheInlineData(string input)
     {
         Assert.True(Challenge5.SomeRandomCode(input));
