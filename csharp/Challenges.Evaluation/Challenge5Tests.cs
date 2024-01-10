0ef2c030680618fe525314d95711d5ef903b6716
Vijay Saayi <Vijay.Saayi@microsoft.com>
2024-01-10 15:38:41 +0530

    Adding comments

--- csharp/Challenges.Evaluation/Challenge5Tests.cs
+++ csharp/Challenges.Evaluation/Challenge5Tests.cs
@@ -2,6 +2,7 @@
 
 public class Challenge5Tests
 {
+    // Goal : What should be the input so that this test case passes ?
     [Theory]
     [InlineData("Test-Case-1 : Provide-some-input")]
     [InlineData("Test-Case-2 : Provide-some-input")]
